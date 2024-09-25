95e6bbbe68fe7187e9071ed0f5c5d01ac04f97ce
distiya <sasithaniranjana@gmail.com>
2024-09-26 00:09:17 +0530

    Modified

--- firstdag.py
+++ firstdag.py
@@ -16,22 +16,21 @@ test_connection = BaseHook.get_connection("test-connection")
 
 envVars = {'TEST_USER':datetime.now(),'TEST_PASSWORD':test_connection.password}
 
-list_dates = ['2024-04-13', '2024-05-13']
+list_dates = ['2024-04-13', '2024-05-13']   
 
-def _process_dates(**kwargs):
-	dr: DagRun = kwargs["dag_run"]
-	params = dr.conf["params"]
-	startDate = date_object = datetime.strptime(params["startDate"], "%Y-%m-%d").date()
-	endDate = date_object = datetime.strptime(params["endDate"], "%Y-%m-%d").date()
-	dates = []
-	delta = timedelta(days=1)
-	while startDate <= endDate:
-	    dates.append(startDate.strftime('%Y-%m-%d'))
-	    startDate += delta
-	return dates    
+with DAG('first_dag_10',start_date=datetime(2024,9,23),schedule_interval=None,catchup=False, params={"startDate":Param((date.today() - timedelta(days=1)).strftime('%Y-%m-%d'), type="string", format="date"),"endDate":Param(date.today().strftime('%Y-%m-%d'), type="string", format="date")}) as dag:
 
-with DAG('first_dag_9',start_date=datetime(2024,9,23),schedule_interval=None,catchup=False, params={"startDate":Param((date.today() - timedelta(days=1)).strftime('%Y-%m-%d'), type="string", format="date"),"endDate":Param(date.today().strftime('%Y-%m-%d'), type="string", format="date")}) as dag:
-
-	process_dates = PythonOperator(task_id='process_first',python_callable=_process_dates,provide_context=True)
+	@task
+	def process_dates(**kwargs):
+		dr: DagRun = kwargs["dag_run"]
+		params = dr.conf["params"]
+		startDate = date_object = datetime.strptime(params["startDate"], "%Y-%m-%d").date()
+		endDate = date_object = datetime.strptime(params["endDate"], "%Y-%m-%d").date()
+		dates = []
+		delta = timedelta(days=1)
+		while startDate <= endDate:
+		    dates.append(startDate.strftime('%Y-%m-%d'))
+		    startDate += delta
+		return dates 
 	
-	process_dates >> parent_group(process_dates.output)
+	parent_group(process_dates())

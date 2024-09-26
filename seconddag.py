9760c566ecfa38827fefe1927e0533eccf805db1
distiya <sasithaniranjana@gmail.com>
2024-09-26 12:19:42 +0530

    Corrected

---
+++ seconddag.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from datetime import date, datetime, timedelta
+from airflow.operators.python import PythonOperator
+from airflow.models import Connection
+from airflow.providers.cncf.kubernetes.operators.job import KubernetesJobOperator
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from airflow.hooks.base import BaseHook
+from airflow.models.param import Param
+from taskgroup import etl_tasks
+from airflow.decorators import dag, task, task_group
+from airflow.models.taskinstance import TaskInstance
+from airflow.models.dagrun import DagRun
+
+#test_connection = Connection.get_connection_from_secrets("test-connection")
+test_connection = BaseHook.get_connection("test-connection")
+
+envVars = {'TEST_USER':datetime.now(),'TEST_PASSWORD':test_connection.password}
+
+list_dates = ['2024-04-13', '2024-05-13']   
+
+with DAG('second_dag_1',start_date=datetime(2024,9,23),schedule_interval=None,catchup=False, params={"startDate":Param(date.today() - timedelta(days=1), type="date"),"endDate":Param(date.today(), type="date")}) as dag:
+
+	@task
+	def process_dates(params: dict) -> list[str]:
+		startDate = params["startDate"]
+		endDate = params["endDate"]
+		dates = []
+		delta = timedelta(days=1)
+		while startDate <= endDate:
+		    dates.append(startDate.strftime('%Y-%m-%d'))
+		    startDate += delta
+		return dates
+	
+	etl_tasks.expand(report_date=process_dates())

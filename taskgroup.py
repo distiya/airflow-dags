901b66afb61211ae92ec9f3645cd7d8dd16d08f6
distiya <sasithaniranjana@gmail.com>
2024-09-26 11:51:17 +0530

    Corrected

--- taskgroup.py
+++ taskgroup.py
@@ -22,7 +22,7 @@ def _process_director(ti):
 def _process_databricks(**kwargs):
 	ti = kwargs['ti']
 	return_string = ti.xcom_pull(task_ids="etl_tasks.job-task", key="return_value")
-	returnType = type(return_string)
+	returnType = type(return_string[0])
 	print(f"Pulled XCom from KubernetesPodOperator: {returnType}")
 	returnStatus = json.loads(return_string)	
 	if(returnStatus["status"] == 0):
